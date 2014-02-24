2baf2d23fbc40eafa081bb7173945a7312599af4
atulnk <atulnk1@gmail.com>
2014-02-24 15:59:42 +0800

    added back the basic attributes, getters and setters to the TaskCards class

--- src/TaskCards.java
+++ src/TaskCards.java
@@ -1,4 +1,59 @@
 
 public class TaskCards {
+	//should have the following TaskName, TaskType, TaskStartTime, TaskEndTime, TaskDate
+	private String TaskName;
+	private String TaskType;
+	private int TaskDate;
+	private int TaskStartTime = 0;
+	private int TaskEndTime = 0;
+	
+	
+	
+	public String getTaskName() {
+		return TaskName;
+	}
 
+	public void setTaskName(String taskName) {
+		TaskName = taskName;
+	}
+
+	public String getTaskType() {
+		return TaskType;
+	}
+
+	public void setTaskType(String taskType) {
+		TaskType = taskType;
+	}
+
+	public int getTaskDate() {
+		return TaskDate;
+	}
+
+	public void setTaskDate(int taskDate) {
+		TaskDate = taskDate;
+	}
+
+	public int getTaskStartTime() {
+		return TaskStartTime;
+	}
+
+	public void setTaskStartTime(int taskStartTime) {
+		TaskStartTime = taskStartTime;
+	}
+
+	public int getTaskEndTime() {
+		return TaskEndTime;
+	}
+
+	public void setTaskEndTime(int taskEndTime) {
+		TaskEndTime = taskEndTime;
+	}
+
+	
+	public TaskCards(){
+		
+	}
+	
+	
 }
+
