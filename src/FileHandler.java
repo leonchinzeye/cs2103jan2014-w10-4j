0e31899941792a306e39a68d9b502bb3a8b8a3c7
omrkhld <omar.khalid.yahya@gmail.com>
2014-02-24 15:16:55 +0800

    no message

---
+++ src/FileHandler.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileHandler {
+
+	private static ArrayList<TaskCards> fileDetails;
+	private static String fileName = null;
+	private static int numberOfTasks = 0;
+	
+	public FileHandler(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	public static void openFile() {
+		
+	}
+	
+	public static void saveFile() {
+		
+	}
+}
