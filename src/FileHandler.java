90671dc2c361d95d8c887f419513826a1df6b4ca
leonchin <leonchin@localhost>
2014-02-24 15:06:55 +0800

    removed unnecessary packages for FileHandler

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Scanner;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
