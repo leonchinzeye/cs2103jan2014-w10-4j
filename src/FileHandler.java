27b41186f90a3eb001387c97fdb3e72c69ce0d91
atulnk <atulnk1@gmail.com>
2014-02-24 15:59:42 +0800

    added back the basic attributes, getters and setters to the TaskCards class

---
+++ src/FileHandler.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileHandler {
+
+	private static ArrayList<TaskCards> fileDetails;
+	private static String uncompletedTasksStorageFileName = "uncompletedTasks.txt";
+	private static String completedTasksStorageFileName = "completedTasks.txt";
+	private static int numberOfUncompletedTasks = 0;
+	private static int numberOfCompletedTasks = 0;
+	
+	public FileHandler() {
+		
+	}
+	
+	public static void openFile() {
+		
+	}
+	
+	public static void saveFile() {
+		
+	}
+}
