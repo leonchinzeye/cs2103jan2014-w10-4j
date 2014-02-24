30c60704ee07bafe781016838fe06e06e54fe659
omrkhld <omar.khalid.yahya@gmail.com>
2014-02-24 14:24:53 +0800

    Imported packages to FileHandler class.

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileHandler {
 
