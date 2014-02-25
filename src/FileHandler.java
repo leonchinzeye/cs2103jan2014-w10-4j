315ede6f3c32c53022ac7b706e72601c16b7b4b2
omar.khalid.yahya <omar.khalid.yahya@gmail.com>
2014-02-25 16:04:33 +0800

    Refined CommandHandler class, changed commandType checks to ".contains". Added Add class with main executeAdd method.

---
+++ src/FileHandler.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileHandler {
+
+	//2 separate arraylists for handling the completed and incomplete tasks
+	private static ArrayList<TaskCard> incompleteTasks;
+	private static ArrayList<TaskCard> completedTasks;
+	
+	//2 separate files for storage. one for incomplete tasks, the other for archiving
+	private static final String INCOMPLETE_TASKS_STORAGE_FILE_NAME = "incompletetasks.txt";
+	private static final String COMPLETED_TASKS_STORAGE_FILE_NAME = "completedtasks.txt";
+	
+	private static int numberOfIncompleteTasks = 0;
+	private static int numberOfCompletedTasks = 0;
+	
+	public FileHandler() {
+		loadFileDetails();
+	}
+	
+	private void loadFileDetails() {
+		openCompletedStorageFile();
+		openIncompleteStorageFile();
+	}
+
+	/*
+	 * When writing to the program, will instantiate a new TaskCard object with the parameters
+	 * filled and then it will be added to the ArrayList of completed/incomplete tasks
+	 */
+	private static void openCompletedStorageFile() {
+		try {
+			FileReader fileRead = new FileReader(COMPLETED_TASKS_STORAGE_FILE_NAME);
+			BufferedReader buffRead = new BufferedReader(fileRead);
+			
+			int numberOfTaskCards = Integer.parseInt(buffRead.readLine());
+			
+			TaskCard task = new TaskCard();
+			ArrayList<String> taskDetails = new ArrayList<String>();
+			
+			for(int i = 0; i < numberOfTaskCards; i++) {
+				taskDetails = getTaskDetailsFromFile(buffRead);
+				
+				String name = buffRead.readLine();
+				String Type = buffRead.readLine();
+				
+				int startDate = Integer.parseInt(buffRead.readLine());
+				int startMonth = Integer.parseInt(buffRead.readLine());
+				int startYear = Integer.parseInt(buffRead.readLine());
+				
+				int endDate = Integer.parseInt(buffRead.readLine());
+				int endMonth = Integer.parseInt(buffRead.readLine());
+				int endYear = Integer.parseInt(buffRead.readLine());
+				
+				int startTime = Integer.parseInt(buffRead.readLine());
+				int endTime = Integer.parseInt(buffRead.readLine());
+				
+				String frequency = buffRead.readLine();
+				int priority = Integer.parseInt(buffRead.readLine());
+			}
+		} catch (FileNotFoundException ex) {
+			createFile(COMPLETED_TASKS_STORAGE_FILE_NAME);
+		} catch (IOException ex) {
+			
+		}
+	}
+	
+	private static ArrayList<String> getTaskDetailsFromFile(
+			BufferedReader buffRead) {
+		ArrayList<String> taskDetails = new ArrayList<String>();
+		
+		
+		return null;
+	}
+
+	private static void openIncompleteStorageFile() {
+		
+	}
+	
+	private static void createFile(String completedTasksStorageFileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void saveFile() {
+		
+	}
+}
---
+++ src/TaskCard.java
@@ -0,0 +1,173 @@
+
+public class TaskCard {
+	
+	//should have the following name, type, startTime, endTime, startDate, endDate, startMonth, endMonth
+	//startYear, endYear, priority, frequency 
+	
+	private String name;
+	private String type;
+	private int startDate;
+	private int endDate;
+	private int startMonth;
+	private int endMonth;
+	private int startYear;
+	private int endYear;
+	private int startTime = 0;
+	private int endTime = 0;
+	private int priority = 0;
+	private String frequency = "";
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getType() {
+		return type;
+	}
+
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+
+	public int getStartDate() {
+		return startDate;
+	}
+
+
+
+	public void setStartDate(int startDate) {
+		this.startDate = startDate;
+	}
+
+
+
+	public int getEndDate() {
+		return endDate;
+	}
+
+
+
+	public void setEndDate(int endDate) {
+		this.endDate = endDate;
+	}
+
+
+
+	public int getStartMonth() {
+		return startMonth;
+	}
+
+
+
+	public void setStartMonth(int startMonth) {
+		this.startMonth = startMonth;
+	}
+
+
+
+	public int getEndMonth() {
+		return endMonth;
+	}
+
+
+
+	public void setEndMonth(int endMonth) {
+		this.endMonth = endMonth;
+	}
+
+
+
+	public int getStartYear() {
+		return startYear;
+	}
+
+
+
+	public void setStartYear(int startYear) {
+		this.startYear = startYear;
+	}
+
+
+
+	public int getEndYear() {
+		return endYear;
+	}
+
+
+
+	public void setEndYear(int endYear) {
+		this.endYear = endYear;
+	}
+
+
+
+	public int getStartTime() {
+		return startTime;
+	}
+
+
+
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+
+
+
+	public int getEndTime() {
+		return endTime;
+	}
+
+
+
+	public void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+
+
+
+	public int getPriority() {
+		return priority;
+	}
+
+
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+
+
+	public String getFrequency() {
+		return frequency;
+	}
+
+
+
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+
+
+
+
+	
+	
+	public TaskCard(){
+		
+	}
+	
+	
+}
+
