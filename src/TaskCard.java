0f81dfd6478499e3551ed06559be535144a47249
atulnk <atulnk1@gmail.com>
2014-02-25 15:21:02 +0800

    expanded on the previous taskStartDate and taskEndDate by breaking them down into startDate, endDate, startMonth, endMonth, startYear, endYear

--- src/TaskCard.java
+++ src/TaskCard.java
@@ -1,80 +1,168 @@
 
 public class TaskCard {
-	//should have the following taskName, taskType, taskStartTime, taskEndTime, taskStartDate,taskEndDate
-	//taskPriority, taskFrequency 
-	private String taskName;
-	private String taskType;
-	private int taskStartDate;
-	private int taskEndDate;
-	private int taskStartTime = 0;
-	private int taskEndTime = 0;
-	private int taskPriority = 0;
-	private String taskFrequency = "";
-
-	public int getTaskPriority() {
-		return taskPriority;
-	}
-
-	public void setTaskPriority(int taskPriority) {
-		this.taskPriority = taskPriority;
-	}
-
-	public String getTaskName() {
-		return taskName;
-	}
-
-	public void setTaskName(String taskName) {
-		this.taskName = taskName;
-	}
-
-	public String getTaskType() {
-		return taskType;
-	}
-
-	public void setTaskType(String taskType) {
-		this.taskType = taskType;
-	}
-
-	public int getStartTaskDate() {
-		return taskStartDate;
-	}
-
-	public void setStartTaskDate(int taskDate) {
-		this.taskStartDate = taskDate;
-	}
 	
-	public int getEndTaskDate() {
-		return taskEndDate;
-	}
-
-	public void setEndTaskDate(int taskDate) {
-		this.taskEndDate = taskDate;
-	}
-
-	public int getTaskStartTime() {
-		return taskStartTime;
-	}
-
-	public void setTaskStartTime(int taskStartTime) {
-		this.taskStartTime = taskStartTime;
-	}
-
-	public int getTaskEndTime() {
-		return taskEndTime;
-	}
-
-	public void setTaskEndTime(int taskEndTime) {
-		this.taskEndTime = taskEndTime;
-	}
+	//should have the following name, type, startTime, endTime, startDate, endDate, startMonth, endMonth
+	//startYear, endYear, priority, frequency 
 	
-	public String getTaskFrequency() {
-		return taskFrequency;
+	private String name;
+	private String type;
+	private int startDate;
+	private int endDate;
+	private int startMonth;
+	private int endMonth;
+	private int startYear;
+	private int endYear;
+	private int startTime = 0;
+	private int endTime = 0;
+	private int priority = 0;
+	private String frequency = "";
+
+	public String getName() {
+		return name;
 	}
 
-	public void setTaskFrequency(String taskFrequency) {
-		this.taskFrequency = taskFrequency;
+
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
+
+
+	public String getType() {
+		return type;
+	}
+
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+
+	public int getStartDate() {
+		return startDate;
+	}
+
+
+
+	public void setStartDate(int startDate) {
+		this.startDate = startDate;
+	}
+
+
+
+	public int getEndDate() {
+		return endDate;
+	}
+
+
+
+	public void setEndDate(int endDate) {
+		this.endDate = endDate;
+	}
+
+
+
+	public int getStartMonth() {
+		return startMonth;
+	}
+
+
+
+	public void setStartMonth(int startMonth) {
+		this.startMonth = startMonth;
+	}
+
+
+
+	public int getEndMonth() {
+		return endMonth;
+	}
+
+
+
+	public void setEndMonth(int endMonth) {
+		this.endMonth = endMonth;
+	}
+
+
+
+	public int getStartYear() {
+		return startYear;
+	}
+
+
+
+	public void setStartYear(int startYear) {
+		this.startYear = startYear;
+	}
+
+
+
+	public int getEndYear() {
+		return endYear;
+	}
+
+
+
+	public void setEndYear(int endYear) {
+		this.endYear = endYear;
+	}
+
+
+
+	public int getStartTime() {
+		return startTime;
+	}
+
+
+
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+
+
+
+	public int getEndTime() {
+		return endTime;
+	}
+
+
+
+	public void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+
+
+
+	public int getPriority() {
+		return priority;
+	}
+
+
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+
+
+	public String getFrequency() {
+		return frequency;
+	}
+
+
+
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+
+
+
+
+	
 	
 	public TaskCard(){
 		
