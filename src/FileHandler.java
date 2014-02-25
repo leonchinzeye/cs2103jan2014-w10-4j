21a2ffab864e8605f96c5654cebff6ef7a3df42e
atulnk <atulnk1@gmail.com>
2014-02-25 14:56:51 +0800

    added some comments on TaskCard.java

---
+++ src/FileHandler.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileHandler {
+
+	//2 separate arraylists for handling the completed and incomplete tasks
+	private static ArrayList<TaskCard> incompleteTasks;
+	private static ArrayList<TaskCard> completedTasks;
+	
+	//2 separate files for storage. one for incomplete tasks, the other for archiving
+	private static final String INCOMPLETE_TASKS_STORAGE_FILE_NAME = "incompletetasks.txt";
+	private static final String COMPLETED_TASKS_STORAGE_FILE_NAME = "completedtasks.txt";
+	
+	private static int numberOfIncompleteTasks = 0;
+	private static int numberOfCompletedTasks = 0;
+	
+	public FileHandler() {
+		loadFileDetails();
+	}
+	
+	private void loadFileDetails() {
+		openCompletedStorageFile();
+		openIncompleteStorageFile();
+	}
+
+	/*
+	 * When writing to the program, will instantiate a new TaskCard object with the parameters
+	 * filled and then it will be added to the ArrayList of completed/incomplete tasks
+	 */
+	private static void openCompletedStorageFile() {
+		try {
+			FileReader fileRead = new FileReader(COMPLETED_TASKS_STORAGE_FILE_NAME);
+			BufferedReader buffRead = new BufferedReader(fileRead);
+			
+			int numberOfTaskCards = Integer.parseInt(buffRead.readLine());
+			
+			
+			for(int i = 0; i < numberOfTaskCards; i++) {
+				
+			}
+		} catch (FileNotFoundException ex) {
+			createFile(COMPLETED_TASKS_STORAGE_FILE_NAME);
+		} catch (IOException ex) {
+			
+		}
+	}
+	
+	private static void createFile(String completedTasksStorageFileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void openIncompleteStorageFile() {
+		
+	}
+	
+	public static void saveFile() {
+		
+	}
+}
---
+++ src/TaskCard.java
@@ -0,0 +1,85 @@
+
+public class TaskCard {
+	//should have the following taskName, taskType, taskStartTime, taskEndTime, taskStartDate,taskEndDate
+	//taskPriority, taskFrequency 
+	private String taskName;
+	private String taskType;
+	private int taskStartDate;
+	private int taskEndDate;
+	private int taskStartTime = 0;
+	private int taskEndTime = 0;
+	private int taskPriority = 0;
+	private String taskFrequency = "";
+
+	public int getTaskPriority() {
+		return taskPriority;
+	}
+
+	public void setTaskPriority(int taskPriority) {
+		this.taskPriority = taskPriority;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getTaskType() {
+		return taskType;
+	}
+
+	public void setTaskType(String taskType) {
+		this.taskType = taskType;
+	}
+
+	public int getStartTaskDate() {
+		return taskStartDate;
+	}
+
+	public void setStartTaskDate(int taskDate) {
+		this.taskStartDate = taskDate;
+	}
+	
+	public int getEndTaskDate() {
+		return taskEndDate;
+	}
+
+	public void setEndTaskDate(int taskDate) {
+		this.taskEndDate = taskDate;
+	}
+
+	public int getTaskStartTime() {
+		return taskStartTime;
+	}
+
+	public void setTaskStartTime(int taskStartTime) {
+		this.taskStartTime = taskStartTime;
+	}
+
+	public int getTaskEndTime() {
+		return taskEndTime;
+	}
+
+	public void setTaskEndTime(int taskEndTime) {
+		this.taskEndTime = taskEndTime;
+	}
+	
+	public String getTaskFrequency() {
+		return taskFrequency;
+	}
+
+	public void setTaskFrequency(String taskFrequency) {
+		this.taskFrequency = taskFrequency;
+	}
+
+	
+	public TaskCard(){
+		
+	}
+	
+	
+}
+
