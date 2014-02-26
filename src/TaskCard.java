eef01a33743b848b63abda3afe19990928a6c008
leonchin <leonchin@localhost>
2014-02-26 11:17:47 +0800

    changed the indentation for TaskCard

--- src/TaskCard.java
+++ src/TaskCard.java
@@ -21,145 +21,100 @@ public class TaskCard {
 		return name;
 	}
 
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
 
 	public String getType() {
 		return type;
 	}
 
-
-
 	public void setType(String type) {
 		this.type = type;
 	}
-
-
 
 	public int getStartDate() {
 		return startDate;
 	}
 
-
-
 	public void setStartDate(int startDate) {
 		this.startDate = startDate;
 	}
-
-
 
 	public int getEndDate() {
 		return endDate;
 	}
 
-
-
 	public void setEndDate(int endDate) {
 		this.endDate = endDate;
 	}
-
-
 
 	public int getStartMonth() {
 		return startMonth;
 	}
 
-
-
 	public void setStartMonth(int startMonth) {
 		this.startMonth = startMonth;
 	}
-
-
 
 	public int getEndMonth() {
 		return endMonth;
 	}
 
-
-
 	public void setEndMonth(int endMonth) {
 		this.endMonth = endMonth;
 	}
-
-
 
 	public int getStartYear() {
 		return startYear;
 	}
 
-
-
 	public void setStartYear(int startYear) {
 		this.startYear = startYear;
 	}
-
-
 
 	public int getEndYear() {
 		return endYear;
 	}
 
-
-
 	public void setEndYear(int endYear) {
 		this.endYear = endYear;
 	}
-
-
 
 	public int getStartTime() {
 		return startTime;
 	}
 
-
-
 	public void setStartTime(int startTime) {
 		this.startTime = startTime;
 	}
-
-
 
 	public int getEndTime() {
 		return endTime;
 	}
 
-
-
 	public void setEndTime(int endTime) {
 		this.endTime = endTime;
 	}
+
 	public String getFrequency() {
 		return frequency;
 	}
-
-
 
 	public void setFrequency(String frequency) {
 		this.frequency = frequency;
 	}
 
-	
 	public int getPriority() {
 		return priority;
 	}
-
-
 
 	public void setPriority(int priority) {
 		this.priority = priority;
 	}
 
-		
 	public TaskCard(){
 		
 	}
-	
-	
 }
 
