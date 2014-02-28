9e2fdaaa33b1c070ba2bd3c215ff9e9130561664
leonchin <leonchin@localhost>
2014-02-28 20:18:05 +0800

    updated general functions in Clear  class

--- src/Clear.java
+++ src/Clear.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Clear {
 
+	private static final int FIRST_ARGUMENT = 0;
 	private static FileHandler fh;
 	
 	public Clear(FileHandler fh) {
@@ -9,9 +10,27 @@ public class Clear {
 	}
 
 	public static void executeClear(String[] userInput) {
+		boolean correctCmd;
 		
+		correctCmd = checkCmdInput(userInput[FIRST_ARGUMENT]);
+		
+		if(correctCmd == true) {
+			
+		} else {
+			
+		}
 	}
 	
+	private static boolean checkCmdInput(String cmd) {
+		String cmdLowerCase = cmd.toLowerCase();
+		
+		if(cmdLowerCase.equals("/clear")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	private static void clearErrorHandling() {
 		
 	}
