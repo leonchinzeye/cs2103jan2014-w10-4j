7bd207703d6c8f192659072f2351d8fcb21b4a57
atulnk <atulnk1@gmail.com>
2014-02-28 19:48:39 +0800

    did some refactoring of BasicGUI.java

--- src/BasicGUI.java
+++ src/BasicGUI.java
@@ -11,99 +11,92 @@ import java.awt.event.ActionListener;
 
 public class BasicGUI{	
 
+	//JTextArea
+	private static JTextArea consoleArea;
 	//JTextField
-	private static JTextArea consoleField;
 	private static JTextField commandField;
-	
+	//JFrame 
+	private static JFrame mainFrame;
 	//JScrollPane 
 	private static JScrollPane consoleScroll;
-	
+	//Container 
+	private static Container storageContainer;
+	//JPanel 
+	private static JPanel consolePanel;
+	private static JPanel commandPanel;
+
 	public static void main(String args[]){
-		
+
 		//create the frame with which the program can work on 
-		JFrame mainFrame = new JFrame();
+		mainFrame = new JFrame();
 		mainFrame.setSize(400, 400);
 		mainFrame.setVisible(true);
 		mainFrame.setResizable(true);
 		mainFrame.setTitle("TaskWorthy");
 		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
+
 		//contain what ever you want to add into the GUI
-		Container storageContainer = mainFrame.getContentPane();
+		storageContainer = mainFrame.getContentPane();
 		storageContainer.setLayout(null);
-		
+
 		//JPanel for display
-		JPanel consolePanel = new JPanel(null);
+		consolePanel = new JPanel(null);
 		consolePanel.setBorder(BorderFactory.createTitledBorder("Console"));
-		
+
 		//JPanel for command
-		JPanel commandPanel = new JPanel(null);
+		commandPanel = new JPanel(null);
 		commandPanel.setBorder(BorderFactory.createTitledBorder("Command"));
-		
+
 		//JTextArea
-		consoleField = new JTextArea();
+		consoleArea = new JTextArea();
 		//JTextField
 		commandField = new JTextField();
-		
+
 		//JLabel
 		//JLabel mainLabel = new JLabel("Hello");
-		
+
 		//Storage container 
 		storageContainer.setBounds(0,0, 400, 400);
 		storageContainer.add(consolePanel);
 		storageContainer.add(commandPanel);
-		
+
 		//scroll function 
-		consoleScroll = new JScrollPane(consoleField);
-		
+		consoleScroll = new JScrollPane(consoleArea);
+
 		//displayPanel
 		consolePanel.setBounds(5,5,375, 275);
 		//part of the display box 
 		consolePanel.add(consoleScroll);
 		consoleScroll.setBounds(8, 20, 360, 245);
-		consoleField.setEditable(false);
-		consoleField.setBackground(Color.white);
+		consoleArea.setEditable(false);
+		consoleArea.setBackground(Color.white);
 		//commandPanel
 		commandPanel.setBounds(5, 280, 375, 70);
 		commandPanel.add(commandField);
 		commandField.setBounds(8, 20, 360, 40); 
-		
-		
-		
+
+
+
 		//input/output field
 		//input into the command field can be displayed on Console
-		
-		
-		
-			commandField.addActionListener(new ActionListener() {
-				@Override
-				
-				public void actionPerformed(ActionEvent arg0) {
-					if(consoleField.getText().isEmpty()){
-						String[] message = commandField.getText().split(" ", 2);
-						consoleField.setText("added " + message[1]);
-						commandField.setText("");
-					}
-					else{
+
+		commandField.addActionListener(new ActionListener() {
+			@Override
+
+			public void actionPerformed(ActionEvent arg0) {
+				if(consoleArea.getText().isEmpty()){
 					String[] message = commandField.getText().split(" ", 2);
-					consoleField.setText(consoleField.getText() + "\nadded " +message[1]);
+					consoleArea.setText("added " + message[1]);
 					commandField.setText("");
-					}
 				}
-			});
-			
-		
-		
-		
-		
-		/*
-		part of the command box
-		mainPanel.add(commadField);
-		commadField.setBounds(180, 118, 360, 180);
-		
-		displayPanel.add(mainLabel);
-		mainLabel.setBounds(5, 20, 50, 20);		
-		 */
+				else{
+					String[] message = commandField.getText().split(" ", 2);
+					consoleArea.setText(consoleArea.getText() + "\nadded " +message[1]);
+					commandField.setText("");
+				}
+			}
+		});
+
 	}
-	
+
 }
