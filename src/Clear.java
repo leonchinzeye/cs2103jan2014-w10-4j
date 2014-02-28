9c70ee0d4712dae5db2feb8f2a23b622045ac658
leonchin <leonchin@localhost>
2014-02-28 21:09:07 +0800

    created hash table for clear function to easily identify the clear commands

--- src/Clear.java
+++ src/Clear.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Clear {
 
-	private static final int FIRST_ARGUMENT = 0;
-	private static FileHandler fh;
+	private static final int CLEAR_INCOMPLETE = 1;
+	private static final int CLEAR_ALL = 2;
+	private static final int CLEAR_EVENTS = 3;
+	private static final int CLEAR_TASKS = 4;
+	private static final int CLEAR_HISTORY = 5;
 	
+	private static final int FIRST_ARGUMENT = 0;
+	
+	private static HashMap<String, Integer> commandTable;
 	/*
 	 * Clear constructor
 	 */
 	public Clear(FileHandler fh) {
-		this.fh = fh;
+		initialiseCmdTypes();
 	}
 
 	public static void executeClear(String[] userInput) {
@@ -18,6 +25,7 @@ public class Clear {
 		correctCmd = checkCmdInput(userInput[FIRST_ARGUMENT]);
 		
 		if(correctCmd == true) {
+			
 			
 		} else {
 			clearErrorHandling();
@@ -31,10 +39,7 @@ public class Clear {
 	private static boolean checkCmdInput(String cmd) {
 		String cmdLowerCase = cmd.toLowerCase();
 		
-		if(cmdLowerCase.equals("/clearh") || 
-				cmdLowerCase.equals("/clearall") || 
-				cmdLowerCase.equals("/cleare") ||
-				cmdLowerCase.equals("/cleart")) {
+		if(commandTable.containsKey(cmdLowerCase)) {
 			return true;
 		} else {
 			return false;
@@ -49,6 +54,21 @@ public class Clear {
 	}
 	
 	private static void clearHistory() {
+		FileHandler.initialiseFileDetails(FileHandler.COMPLETED_TASKS_STORAGE_FILE_NAME);
+		FileHandler.writeCompleteTasksFile();
+	}
+	
+	private static void clearAll() {
+		FileHandler.initialiseFileDetails(FileHandler.INCOMPLETE_TASKS_STORAGE_FILE_NAME);
+		FileHandler.writeIncompleteTasksFile();
+	}
+	
+	private static void initialiseCmdTypes() {
+		commandTable.put("/clear", CLEAR_INCOMPLETE);
+		commandTable.put("/clearall", CLEAR_ALL);
+		commandTable.put("/cleare", CLEAR_EVENTS);
+		commandTable.put("cleart", CLEAR_TASKS);
+		commandTable.put("/clearh", CLEAR_HISTORY);
 		
 	}
 }
