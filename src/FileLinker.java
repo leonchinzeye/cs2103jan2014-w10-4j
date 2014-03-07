f29c00e1e22d123967443c0887ee91e1ee6985dd
leonchin <leonchin@localhost>
2014-03-07 15:21:19 +0800

    created a new class that acts as a link between the file storage and the logic classes

---
+++ src/FileLinker.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+/**
+ * Class that links the individual logic components to the FileHandler
+ * and vice versa
+ * @author leon
+ *
+ */
+public class FileLinker {
+
+	public FileLinker() {
+		
+	}
+	
+	/**
+	 * method that add logic will call to update modified data to the file
+	 * @param arrayToBeUpdated
+	 */
+	public static void addHandling(ArrayList<TaskCard> arrayToBeUpdated) {
+		
+	}
+	
+	/**
+	 * method that delete logic will call to update modified data to the file
+	 * @param arrayToBeUpdated
+	 */
+	public static void deleteHandling(ArrayList<TaskCard> arrayToBeUpdated) {
+		
+	}
+	
+	/**
+	 * method for edit logic that will update modified data to the file
+	 * @param arrayToBeUpdated
+	 */
+	public static void editHandling(ArrayList<TaskCard> arrayToBeUpdated) {
+		
+	}
+}
