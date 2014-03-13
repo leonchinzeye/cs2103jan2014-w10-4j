72a3460798e399fe3b9b2849c4b855cb8b84e90c
leonchin <leonchin@localhost>
2014-03-13 15:17:27 +0800

    created a new Data UI class for GUI parsing

---
+++ src/DataUI.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class DataUI {
+
+	private ArrayList<TaskCard> incompleteTasks;
+	private ArrayList<TaskCard> incompleteEvents;
+	private ArrayList<TaskCard> completedTasks;
+	private ArrayList<TaskCard> completedEvents;
+}
