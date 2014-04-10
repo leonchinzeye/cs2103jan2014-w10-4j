16fdc80ee94dbbb96cefd20448ecebe166d12ddf
leonchin <leonchin@localhost>
2014-04-10 18:11:19 +0800

    commented on refresh function

--- src/application/RefreshUI.java
+++ src/application/RefreshUI.java
@@ -3,22 +3,14 @@ package application;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-/**
- * this class allows the user to refresh the GUI
- * things it should do:
- *   - update the UI based
- *   - the GUI should call refresh every hour of the clock
- *   - refresh should be able to handle marking of events or tasks once
- *     past the deadline
- *   - when refreshing and marking events that have past their designated
- *  	 timeslots, for those repeating ones, it should know how to put it 
- *  	 to the next repeated time
- * @author leon
- *
- */
 //@author A0097304E
 public class RefreshUI {
 	
+	/**
+	 * this is the main method that is called to refresh the details on the UI.
+	 * the clock shown on the UI is also refreshed at this point
+	 * @return
+	 */
 	public static boolean executeRefresh(FileLinker fileLink, DataUI dataUI) {
 		//should check for events that have passed the designated time (not done yet)		
 		Calendar now = GregorianCalendar.getInstance();
