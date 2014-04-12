47b123ec0699225d0538f02a9ae0bd1f4adb0adb
leonchin <leonchin@localhost>
2014-04-13 01:31:53 +0800

    removed unnecessary stuff in task card

--- src/application/TaskCard.java
+++ src/application/TaskCard.java
@@ -1,26 +1,17 @@
 package application;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 //@author A0097304E
 public class TaskCard implements Cloneable {
 	
-	private static final int MAX_PRIORITY = 3;
 	private String name;
-	/*
-	 * task types: T and FT
-	 * event types: E
-	 */
 	private String type;
 	private int priority = 0;
-	private Calendar current = new GregorianCalendar();
 	private Calendar startDay = new GregorianCalendar();
 	private Calendar endDay = new GregorianCalendar();
-	private SimpleDateFormat dateString = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-	private String taskString;
-	
+
 	public TaskCard(){
 	}
 	
@@ -70,23 +61,6 @@ public class TaskCard implements Cloneable {
 	
 	public void setEndDay(Calendar end) {
 		this.endDay = end;
-	}
-	
-	public String getTaskString() {
-		taskString = "";
-		for (int i = 0; i < MAX_PRIORITY; i++) {
-			if (priority > i) {
-				taskString += "*";
-			} else {
-				taskString += " ";
-			}
-		}
-		if(type.equals("FT") && type.equals("T")){
-			taskString += name + ", " + dateString.format(startDay.getTime()); 
-		} else {
-			taskString += name + ", " + dateString.format(startDay.getTime()) + " - " + dateString.format(endDay.getTime());
-		}		
-		return taskString;
 	}
 	
 	@Override
