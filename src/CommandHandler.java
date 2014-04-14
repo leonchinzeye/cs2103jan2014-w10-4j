d4c628d6aeb26cc2144daed5518a6b3f4571ba9f
leonchin <leonchin@localhost>
2014-04-14 20:54:15 +0800

    major debug on dateandtimeformats along with add tests

---
+++ src/CommandHandler.java
@@ -0,0 +1,176 @@
+//@author A0097304E
+/**
+ * This class works as a command handler.
+ * It parses in the first input and checks if it is a valid command.
+ * If the command is valid, then CommandHandler will pass the inputs
+ * into the respective functions based on the commands that the user
+ * has entered
+ */
+public class CommandHandler {
+	
+	private static FileLinker fileLink;
+	private static DataUI dataUI;
+	
+	private static Delete deleteHandler;
+	private static Search searchHandler;
+	private static Mark markHandler;
+	private static Edit editHandler;
+	private static Undo undoHandler;
+	private static Add addHandler;
+	private static DateAndTimeFormats dateFormats;
+	
+	private static final String MESSAGE_ERROR_INVALID_COMMAND = "It appears you have typed "
+			+ "something wrongly! Please try another command.";
+	
+	public enum COMMAND_TYPE {
+		ADD, DELETE, CLEAR, EDIT, SEARCH, MARK, RESET, EXIT, INVALID, ENTER, UNDO, REDO, HELP, VIEW, THEME, HIGHLIGHT
+	}
+	
+	/**
+	 * Constructor for CommandHandler
+	 */
+	public CommandHandler() {
+		addHandler = new Add();
+		deleteHandler = new Delete();
+		searchHandler = new Search();
+		markHandler = new Mark();
+		editHandler = new Edit();
+		undoHandler = new Undo();
+		dateFormats = new DateAndTimeFormats();
+		
+		fileLink = new FileLinker();
+		dataUI = new DataUI();
+		
+		RefreshUI.executeRefresh(fileLink, dataUI);
+	}
+	
+	/**
+	 * executes the command that the user entered
+	 * @return
+	 * returns a packaged DataUI to the caller of this method
+	 */
+	public DataUI executeCmd(String userInput, int tableNo) {	
+		checkCmdAndPerform(userInput, tableNo);
+		return dataUI;
+	}
+
+	/**
+	 * checks if the command the user entered is valid and calls the
+	 * appropriate classes
+	 */
+	private void checkCmdAndPerform(String userInput, int tableNo) {
+		String[] tokenizedInput = userInput.trim().split("\\s+", 2);
+		
+		String commandTypeString = tokenizedInput[0];
+		COMMAND_TYPE commandType = determineCommandType(commandTypeString);
+		
+		String response = "";
+		switch(commandType) {
+			case ADD:
+				fileLink.resetState();
+				addHandler.executeAdd(userInput, fileLink, dataUI, undoHandler, dateFormats);
+				break;
+			case DELETE:
+				deleteHandler.executeDelete(userInput, fileLink, dataUI, tableNo, undoHandler);
+				break;
+			case EDIT:
+				editHandler.executeEdit(userInput, fileLink, dataUI, tableNo, undoHandler, dateFormats);
+				break;
+			case SEARCH:
+				fileLink.resetState();
+				searchHandler.executeSearch(userInput, fileLink, dataUI, dateFormats);
+				break;
+			case ENTER:
+				fileLink.resetState();
+				RefreshUI.executeRefresh(fileLink, dataUI);
+				dataUI.setFeedback("Read me!");
+				break;
+			case MARK:
+				markHandler.executeMark(userInput, fileLink, dataUI, tableNo, undoHandler);
+				break;
+			case UNDO:
+				response = undoHandler.executeUndo(fileLink);
+				RefreshUI.executeRefresh(fileLink, dataUI);
+				dataUI.setFeedback(response);
+				break;
+			case REDO:
+				response = undoHandler.executeRedo(fileLink);
+				RefreshUI.executeRefresh(fileLink, dataUI);
+				dataUI.setFeedback(response);
+				break;
+			case HELP:
+				dataUI.setFeedback("This will return responses to you based on your commands.");
+				break;
+			case VIEW:
+				dataUI.setFeedback("Read me!");
+				break;
+			case THEME:
+				if (tokenizedInput[1].equalsIgnoreCase("Jedi")) {
+					dataUI.setFeedback("May the Force be with you.");
+				} else if (tokenizedInput[1].equalsIgnoreCase("Sith")) {
+					dataUI.setFeedback("Only a Sith deals in absolutes.");
+				} else if (tokenizedInput[1].equalsIgnoreCase("Australia")) {
+					dataUI.setFeedback("G'day mate!");
+				} else if (tokenizedInput[1].equalsIgnoreCase("Italy")) {
+					dataUI.setFeedback("Mamma mia!");
+				} else {
+					dataUI.setFeedback("We don't have that theme!");
+				}
+				break;
+			case HIGHLIGHT:
+				RefreshUI.executeRefresh(fileLink, dataUI);
+				break;
+			case INVALID:
+				dataUI.setFeedback(MESSAGE_ERROR_INVALID_COMMAND);
+				break;
+			case EXIT:
+				System.exit(0);
+				break;
+			default:
+				break;
+		}
+	}
+	
+	/**
+	 * determines the command that the user entered
+	 * @return
+	 * returns a COMMAND_TYPE indicating what the command the user entered
+	 */
+	private COMMAND_TYPE determineCommandType(String commandTypeString) {
+		commandTypeString = commandTypeString.toLowerCase();
+		
+		if (commandTypeString.contains("add")) {
+			return COMMAND_TYPE.ADD;
+		} else if (commandTypeString.contains("del")) {
+			return COMMAND_TYPE.DELETE;
+		} else if (commandTypeString.contains("edit")) {
+		 	return COMMAND_TYPE.EDIT;
+		} else if (commandTypeString.contains("search")) {
+		 	return COMMAND_TYPE.SEARCH;
+		} else if (commandTypeString.contains("mark")) {
+		 	return COMMAND_TYPE.MARK;
+		} else if (commandTypeString.isEmpty()) {
+			return COMMAND_TYPE.ENTER;
+		} else if(commandTypeString.equals("undo")) {
+			return COMMAND_TYPE.UNDO;
+		} else if(commandTypeString.equals("redo")) {
+			return COMMAND_TYPE.REDO;
+		} else if(commandTypeString.equals("help")) {
+			return COMMAND_TYPE.HELP;
+		} else if(commandTypeString.equals("view")) {
+			return COMMAND_TYPE.VIEW;
+		} else if(commandTypeString.equals("theme")) {
+			return COMMAND_TYPE.THEME;
+		} else if (commandTypeString.equals("/x")) {
+		 	return COMMAND_TYPE.EXIT;
+		} else if (commandTypeString.equals("highlightexpiredandongoingrows")) {
+			return COMMAND_TYPE.HIGHLIGHT;
+		}	else {
+			return COMMAND_TYPE.INVALID;
+		}
+	}
+	
+	public DataUI getDataUI() {
+		return dataUI;
+	}
+}
---
+++ src/DateAndTimeFormats.java
@@ -0,0 +1,290 @@
+//@author A0097304E
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class DateAndTimeFormats {
+
+	private SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+	
+	private SimpleDateFormat[] properDateFormats = {
+		new SimpleDateFormat("dd/MM/yyyy"),
+		new SimpleDateFormat("dd-MM-yyyy"),
+		new SimpleDateFormat("dd MMM yyyy")
+	};
+	
+	private SimpleDateFormat[] lazyYearFormats = {
+		new SimpleDateFormat("dd/MM/yy"),
+		new SimpleDateFormat("dd-MM-yy"),
+		new SimpleDateFormat("dd MMM yy")
+	};
+	
+	private SimpleDateFormat[] lazyDateFormats = {
+		new SimpleDateFormat("dd/MM yyyy"),
+		new SimpleDateFormat("dd-MM yyyy"),
+		new SimpleDateFormat("dd MMM yyyy"),
+		new SimpleDateFormat("ddMMM yyyy"),
+		new SimpleDateFormat("dd/MMM yyyy")
+	};
+	
+	private SimpleDateFormat[] complete24HrFormat = {
+		new SimpleDateFormat("HHmm"),
+		new SimpleDateFormat("HH:mm"),
+		new SimpleDateFormat("HH.mm")
+	};
+	
+	private SimpleDateFormat[] complete12HrFormat = {
+		new SimpleDateFormat("hh:mma"),
+		new SimpleDateFormat("hh.mma"),
+	};
+	
+	private SimpleDateFormat[] hourOnly12HrFormat = {
+		new SimpleDateFormat("hha"),
+	};
+
+	public DateAndTimeFormats() {
+		
+	}
+	
+	/**
+	 * takes in a string and checks if it is a valid 12 hour
+	 * time format with hours only
+	 * Eg 2pm
+	 * @return
+	 * returns a date object if the string is valid, else
+	 * returns a null object
+	 */
+	@SuppressWarnings("deprecation")
+	public Date isHourOnly(String input) {
+		for(int i = 0; i < hourOnly12HrFormat.length; i++) {
+			SimpleDateFormat formatter = hourOnly12HrFormat[i];
+			formatter.setLenient(false);
+			
+			try {
+				ParsePosition pos = new ParsePosition(0);
+				Date userDate = formatter.parse(input, pos);
+				
+				if(pos.getIndex() < input.length()) {
+					throw new ParseException(input, pos.getIndex());
+				}
+				
+				if(userDate == null) {
+					continue;
+				}
+				
+				Calendar enteredDate = GregorianCalendar.getInstance();
+				enteredDate.set(Calendar.HOUR_OF_DAY, userDate.getHours());
+				enteredDate.set(Calendar.MINUTE, 0);
+				enteredDate.set(Calendar.SECOND, 0);
+				enteredDate.set(Calendar.MILLISECOND, 0);
+				
+				return enteredDate.getTime();
+			} catch(ParseException e) {
+				continue;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * takes in a string and checks if it is a valid 12 hour
+	 * time format 
+	 * Eg 2.30pm
+	 * @return
+	 * returns a date object if the string is valid, else
+	 * returns a null object
+	 */
+	@SuppressWarnings("deprecation")
+	public Date isComplete12Hr(String input) {
+		for(int i = 0; i < complete12HrFormat.length; i++) {
+			SimpleDateFormat formatter = complete12HrFormat[i];
+			formatter.setLenient(false);
+			
+			try {
+				ParsePosition pos = new ParsePosition(0);
+				Date userDate = formatter.parse(input, pos);
+				
+				if(pos.getIndex() < input.length()) {
+					throw new ParseException(input, pos.getIndex());
+				}
+				
+				if(userDate == null) {
+					continue;
+				}
+				
+				Calendar enteredDate = GregorianCalendar.getInstance();
+				enteredDate.set(Calendar.HOUR_OF_DAY, userDate.getHours());
+				enteredDate.set(Calendar.MINUTE, userDate.getMinutes());
+				enteredDate.set(Calendar.SECOND, 0);
+				enteredDate.set(Calendar.MILLISECOND, 0);
+				
+				return enteredDate.getTime();
+			} catch(ParseException e) {
+				continue;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * takes in a string and checks if it is a valid 24 hour
+	 * time format 
+	 * Eg 16:35
+	 * @return
+	 * returns a date object if the string is valid, else
+	 * returns a null object
+	 */
+	@SuppressWarnings("deprecation")
+	public Date isComplete24Hr(String input) {
+		for(int i = 0; i < complete24HrFormat.length; i++) {
+			SimpleDateFormat formatter = complete24HrFormat[i];
+			formatter.setLenient(false);
+			
+			try {
+				ParsePosition pos = new ParsePosition(0);
+				Date userDate = formatter.parse(input, pos);
+				
+				if(pos.getIndex() < input.length()) {
+					throw new ParseException(input, pos.getIndex());
+				}
+				
+				if(userDate == null) {
+					continue;
+				}
+				
+				Calendar enteredDate = GregorianCalendar.getInstance();
+				enteredDate.set(Calendar.HOUR_OF_DAY, userDate.getHours());
+				enteredDate.set(Calendar.MINUTE, userDate.getMinutes());
+				enteredDate.set(Calendar.SECOND, 0);
+				enteredDate.set(Calendar.MILLISECOND, 0);
+				
+				return enteredDate.getTime();
+			} catch(ParseException e) {
+				continue;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * takes in a string and checks if it is a valid date
+	 * format without the year entered
+	 * Eg 23 May
+	 * @return
+	 * returns a date object if the string is valid, else
+	 * returns a null object
+	 */
+	public Date isLazyDate(String input) {
+		Date today = new Date();
+		
+		String currentYear = yearFormat.format(today);
+		String userInput = input + " " + currentYear;
+		
+		for(int i = 0; i < lazyDateFormats.length; i++) {
+			SimpleDateFormat formatter = lazyDateFormats[i];
+			formatter.setLenient(false);
+
+			try {
+				ParsePosition pos = new ParsePosition(0);
+				Date userDate = formatter.parse(userInput, pos);
+				
+				if(pos.getIndex() < input.length()) {
+					throw new ParseException(userInput, pos.getIndex());
+				}
+				
+				if(userDate == null) {
+					continue;
+				}
+				
+				return userDate;
+			} catch(ParseException e) {
+				continue;
+			}
+		}
+		
+		return null;
+	}
+
+	/**
+	 * takes in a string and checks if it is a valid date
+	 * format with the year entered as 2 digits
+	 * Eg 23/6/14
+	 * @return
+	 * returns a date object if the string is valid, else
+	 * returns a null object
+	 */
+	public Date isLazyYearDate(String input) {
+		for(int i = 0; i < lazyYearFormats.length; i++) {
+			SimpleDateFormat formatter = lazyYearFormats[i];
+			formatter.setLenient(false);
+			
+			Date startOf21stCentury = new GregorianCalendar(2001, 1, 1).getTime();
+			formatter.set2DigitYearStart(startOf21stCentury);
+			
+			try {
+				ParsePosition pos = new ParsePosition(0);
+				Date userDate = formatter.parse(input, pos);
+				
+				if(pos.getIndex() < input.length()) {
+					throw new ParseException(input, pos.getIndex());
+				}
+				
+				if(userDate == null) {
+					continue;
+				}
+				
+				return userDate;
+			} catch(ParseException e) {
+				continue;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * takes in a string and checks if it is a valid date
+	 * format
+	 * Eg 23/6/2014
+	 * @return
+	 * returns a date object if the string is valid, else
+	 * returns a null object
+	 */
+	public Date isProperDate(String input){
+		String[] array = input.trim().split("/| |-");
+		
+		if(array[array.length - 1].length() != 4) {
+			return null;
+		}
+		
+		for(int i = 0; i < properDateFormats.length; i++) {
+			SimpleDateFormat formatter = properDateFormats[i];
+			formatter.setLenient(false);
+			
+			try {
+				ParsePosition pos = new ParsePosition(0);
+				Date userDate = formatter.parse(input, pos);
+				
+				if(pos.getIndex() < input.length()) {
+					throw new ParseException(input, pos.getIndex());
+				}
+				
+				if(userDate == null) {
+					continue;
+				}
+				
+				return userDate;
+			} catch(ParseException e){
+				continue;
+			}
+		}
+		
+		return null;
+	}
+}
